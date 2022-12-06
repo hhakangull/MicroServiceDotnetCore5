9d2bedee23940a42ba88a8110c9f7bdd42d3413e
Hakan GÜL <hakanngul@icloud.com>
2022-12-06 22:59:10 +0300

    Automapper eklendi

---
+++ Services/Catalog/Microservices.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microservices.Services.Catalog.Models;
+using MicroServices.Services.Catalog.Dtos;
+
+namespace MicroServices.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
--- Services/Catalog/Microservices.Services.Catalog/Program.cs
+++ Services/Catalog/Microservices.Services.Catalog/Program.cs
@@ -2,10 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
